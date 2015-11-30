b52dbd93efa8878815422df40444df030f64f69d
jas857 <jas857@cornell.edu>
2015-11-30 09:52:53 -0500

    added some functions to the mli file to help with starting the game and with testing

--- board.mli
+++ board.mli
@@ -11,4 +11,10 @@ type board = {
 	dcards : dcard list
 }
 
-
+(* Generates a new Board to be used by randomly placing tiles, and randomly
+creating a dcard list, port locations should be fixed *)
+val initialize_board : unit -> board
+(* Generates the randomly placed tile list that makes up the board*)
+val initialize_tiles : unit -> tile list
+(* Generates the stack fo dcards in a random order *)
+val initialize_dcards : unit -> dcard list
