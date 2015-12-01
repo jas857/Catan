6b803801cb3367607e7251886fb4da7fe618eb7b
Kevin Gregor <krg43@cornell.edu>
2015-12-01 10:28:21 -0500

    accept

--- tests.ml
+++ tests.ml
@@ -23,7 +23,11 @@ let newTiles = rebuild_tile_list ranTiles tempDesert
 
 TEST_UNIT = check_for_robber newTiles false === false
 
+
+
 (*Test Dcards *)
+
+
 
 (*Test Gamestate *)
 let ranBoard = initialize_board ()
@@ -74,52 +78,63 @@ let next = change_turn progress2
 TEST_UNIT = next === {prodstate with playerturn = Red}
 
 
-(*Testing for change_turn during start stage,
-need tests for change_player as well *)
-(*
-let startstate = {playerturn = Red; players = initialize_non_ai_players ();
-game_board = ranBoard; game_stage = Start; longest_road_claimed = false;
-largest_army_claimed = false}
+(*Testing for change_player *)
 let redPlayer = init_non_ai_player Red
 let bluePlayer = init_non_ai_player Blue
 let whitePlayer = init_non_ai_player White
 let orangePlayer = init_non_ai_player Orange
 
-let next = change_player (change_turn startstate)
-{redPlayer with settlements_left = 4; roads_left = 14}
+let addRedVP = {redPlayer with victory_points = 1}
+let newPList = [addRedVP;bluePlayer;whitePlayer;orangePlayer]
 
-TEST_UNIT = next === {startstate with playerturn = Blue}
-
-let next2 = change_player (change_turn next)
-{bluePlayer with settlements_left = 4; roads_left = 14}
-TEST_UNIT = next2 === {next with playerturn = White}
-
-let next = change_player (change_turn next2)
-{whitePlayer with settlements_left = 4; roads_left = 14}
-TEST_UNIT = next === {next2 with playerturn = Orange}
-
-let next2 = change_player (change_turn next)
-{orangePlayer with settlements_left = 4; roads_left = 14}
-TEST_UNIT = next2 === {next with playerturn = Orange}
-
-let next = change_player (change_turn next2)
-{orangePlayer with settlements_left = 3; roads_left = 13}
-TEST_UNIT = next === {next2 with playerturn = White}
-
-let next2 = change_player (change_turn next)
-{whitePlayer with settlements_left = 3; roads_left = 13}
-TEST_UNIT = next2 === {next with playerturn = Blue}
-
-let next = change_player (change_turn next2)
-{bluePlayer with settlements_left = 3; roads_left = 13}
-TEST_UNIT = next === {next2 with playerturn = Red}
-
-let newStage = change_player (change_turn next)
-{redPlayer with settlements_left = 3; roads_left = 13}
-TEST_UNIT = newStage === {next with game_stage = Production}
+TEST_UNIT = change_player buildstate addRedVP ===
+            {buildstate with players = newPList}
+(* add some more cases here *)
 
 
-*)
+(*Testing for change_stage during start stage,
+need tests for change_player as well *)
+
+let startstate = {playerturn = Red; players = initialize_non_ai_players ();
+game_board = ranBoard; game_stage = Start; longest_road_claimed = false;
+largest_army_claimed = false}
+
+
+let next = change_stage (change_player startstate
+{redPlayer with settlements_left = 4; roads_left = 14})
+
+TEST_UNIT = next.playerturn === Blue
+
+let next2 = change_stage (change_player next
+{bluePlayer with settlements_left = 4; roads_left = 14})
+TEST_UNIT = next2.playerturn === White
+
+let next = change_stage (change_player next2
+{whitePlayer with settlements_left = 4; roads_left = 14})
+TEST_UNIT = next.playerturn === Orange
+
+let next2 = change_stage (change_player next
+{orangePlayer with settlements_left = 4; roads_left = 14})
+TEST_UNIT = next2.playerturn === Orange
+
+let next = change_stage (change_player next2
+{orangePlayer with settlements_left = 3; roads_left = 13})
+TEST_UNIT = next.playerturn === White
+
+let next2 = change_stage (change_player next
+{whitePlayer with settlements_left = 3; roads_left = 13})
+TEST_UNIT = next2.playerturn === Blue
+
+let next = change_stage (change_player next2
+{bluePlayer with settlements_left = 3; roads_left = 13})
+TEST_UNIT = next.playerturn === Red
+
+let newStage = change_stage (change_player next
+{redPlayer with settlements_left = 3; roads_left = 13})
+TEST_UNIT = newStage.game_stage === Production
+
+
+
 
 
 
